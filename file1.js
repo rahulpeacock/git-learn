f7f7ebc708d2f44ce96862815e0d55af2742972c
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2024-10-01 07:40:13 +0530

    learn git add -p

--- file1.js
+++ file1.js
@@ -1,1 +1,8 @@
 console.log('Hello file1.js - edit')
+
+
+console.log('hello the is a great text - ');
+console.log()
+console.log(
+const j = 'My name is Rahul Palamarthi'
+
--- file2.js
+++ file2.js
@@ -1,3 +1,5 @@
 console.log('Hello file2.js - edit')
 
-// trying to fix the bug
+console.log('This is first commit');
+console.log('This is second commit');
+console.log('This is third commit')
